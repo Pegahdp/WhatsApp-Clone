c85d7a4c7a95751feb3b06da891c8d36a6780c84
Pegah Pasha <git config --global user.name pegahdp>
2023-02-02 11:22:47 +0800

    Fetch Dummy Data Users From API in Chats Component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,11 @@
 import avatar from '../assets/avatar.png'
 
 
-function Chat() {
+function Chat({user}) {
   return (
-    <div className="flex w-full h-20 py-2 px-4 mt-2 ">
-      <img src={avatar} alt="/" className="rounded-full" />
-      <div className="border-b w-full flex items-center px-4">name</div>
+    <div className="flex w-full h-24 px-4 items-center hover:bg-gray-100">
+      <img src={user.avatar} alt="/" className="rounded-full w-16 h-16" />
+      <div className="border-b h-24 w-full flex items-center p-4"> {user.first_name} {user.last_name}</div>
     </div>
   );
 }
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,15 +1,33 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Chat from "./Chat";
 
 function Chats() {
+  const [users, setUsers] = useState([]);
+
+  const fetchUserData = async () => {
+    const response = await axios.get("https://reqres.in/api/users");
+    setUsers(response.data.data);
+  };
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  console.log(users);
   return (
     <div className="flex-col overflow-y-scroll cursor-pointer h-full">
-      <Chat />
-      <Chat />
-      <Chat />
-      <Chat />
-      <Chat />
+      <div>
+       
+        {users.map((user, i) => (
+          <Chat key={i} user={user} />
+        ))}
+      </div>
+      <div>
+       
+       {users.map((user, i) => (
+         <Chat key={i} user={user} />
+       ))}
+     </div>
     </div>
   );
 }
