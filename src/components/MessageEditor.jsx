d3c5b49b27975ea8386a8d20a1c02894d24b2d86
Pegah Pasha <git config --global user.name pegahdp>
2023-02-02 13:57:31 +0800

    Finish MessageEditor Component UI

--- src/components/MessageEditor.jsx
+++ src/components/MessageEditor.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { AiOutlinePaperClip } from "react-icons/ai";
+import { MdSend } from "react-icons/md";
+import { BiHappy } from "react-icons/bi";
+
+
 
 function MessageEditor() {
+  
   return (
-    <div>MessageEditor</div>
+    <div className="flex items-center  bg-gray-300 w-full h-24 gap-3 py-2">
+      <button>
+        <AiOutlinePaperClip
+          size={30}
+          className=" text-gray-500 ml-2 font-bold  hover:text-blue-800 "
+        />
+      </button>
+      <button>
+        <BiHappy
+          size={26}
+          className="text-gray-500 mr-2 hover:text-blue-800  "
+        />
+      </button>
+
+      <input
+        type="text"
+        placeholder="Type a message"
+        className="rounded-lg p-2 w-full outline-0"
+      />
+
+      <button>
+        <MdSend
+          size={26}
+          className="text-gray-500 mr-2 hover:text-blue-800  "
+        />
+      </button>
+    </div>
   )
 }
 
